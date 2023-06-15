a2148270a937c1595ab58c9d0652d76a100b4b42
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-06-15 00:21:18 -0300

    ✨ feat: configura proj
    Co-authored-by: Ikaro Vieira <ikarovieiraa@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MangaModule } from './manga/manga.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MangaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
