0379a0b361de3e27dac689ce9d630bd7419d4e19
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-06-15 00:19:44 -0300

    ✨ feat: Dto do user
    Co-authored-by: Ikaro Vieira <ikarovieiraa@gmail.com>

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsEmail,
+  IsString,
+  IsStrongPassword,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({
+    message: 'O nome deve ser um texto',
+  })
+  @MinLength(3, {
+    message: 'O nome deve ter pelo menos 3 caracteres',
+  })
+  name: string;
+
+  @IsEmail({}, { message: 'Digite um email válido' })
+  email: string;
+
+  @IsStrongPassword(
+    {
+      minLength: 6,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 2,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'A senha deve conter pelo menos 6 caracteres, 1 letra maiúscula, 1 letra minúscula, 2 número e 1 símbolo',
+    },
+  )
+  password: string;
+}
