bfbc32ac33f43f53703c8a79845da71a2e106e29
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-06-15 23:39:00 -0300

    ✨ feat: adc chapterModule
    Co-authored-by: Ikaro Vieira <ikarovieiraa@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MangaModule } from './manga/manga.module';
 import { UserModule } from './user/user.module';
+import { ChapterModule } from './chapter/chapter.module';
 
 @Module({
-  imports: [MangaModule, UserModule],
+  imports: [MangaModule, UserModule, ChapterModule],
   controllers: [AppController],
   providers: [AppService],
 })
