996657cc470c67946c37aaae418502fc9226e01e
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-06-16 23:50:07 -0300

    🐳 chore: fastify / express
    Co-authored-by: Ikaro Vieira <ikarovieiraa@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,19 @@ import { AppService } from './app.service';
 import { MangaModule } from './manga/manga.module';
 import { UserModule } from './user/user.module';
 import { ChapterModule } from './chapter/chapter.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
-  imports: [MangaModule, UserModule, ChapterModule],
+  imports: [MangaModule, UserModule, ChapterModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
+  const app = await NestFactory.create(
     AppModule,
-    new FastifyAdapter(),
+    // new FastifyAdapter(),
   );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001, '0.0.0.0');
