f68b6c5dcabafe29a225573e53d06fead37f6a58
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-06-16 23:48:37 -0300

    ♻️ Refactorr: refatora
    Co-authored-by: Ikaro Vieira <ikarovieiraa@gmail.com>

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,34 +10,40 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // Cadastrar um novo usuário
   @Post()
   async create(@Body() newUser: CreateUserDto) {
     return this.userService.create(newUser); // This will return the newly created user
   }
 
+  // Listar todos os usuários
   @Get()
   async findAll() {
     return this.userService.findAll(); // This will return all the users
   }
 
+  // Listar um usuário específico
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id); // This will return the user with the id passed in the url
   }
 
+  // Atualizar um usuário
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
-    @Body() newUser: CreateUserDto,
+    @Body() newUser: UpdateUserDto,
   ) {
     return this.userService.update(id, newUser); // This will update the user with the id passed in the url
   }
 
+  // Deletar um usuário
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number) {
     return this.userService.remove(id); // This will remove the user with the id passed in the url
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,85 +1,105 @@
 import { PrismaService } from '../prisma/prisma.service';
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
+  public querySelectWithoutPassword = {
+    id: true,
+    name: true,
+    email: true,
+    password: false,
+  };
+
+  async userExists(id: number) {
+    const user = await this.prisma.user.count({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        `Usuário ${id} não existe`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async encryptPassword(password: string) {
+    return hash(password, 10);
+  }
+
   async create({ email, name, password }: CreateUserDto) {
-    const user = await this.prisma.user.findUnique({
+    const user = await this.prisma.user.findFirst({
       where: {
         email,
       },
     });
+
     if (user) {
       throw new HttpException('Usuário já existe', HttpStatus.BAD_REQUEST);
     }
+
     return this.prisma.user.create({
       data: {
         email,
         name,
-        password,
+        password: await this.encryptPassword(password),
       },
+      select: this.querySelectWithoutPassword,
     });
     // This will return the newly created user
   }
 
   async findAll() {
-    return this.prisma.user.findMany(); // This will return all the users
+    return this.prisma.user.findMany({
+      select: this.querySelectWithoutPassword,
+    }); // This will return all the users
   }
 
   async findOne(id: number) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!user) {
-      throw new HttpException('Usuário não existe', HttpStatus.BAD_REQUEST);
-    }
+    await this.userExists(id);
 
     return this.prisma.user.findUnique({
       where: {
         id,
       },
+      select: this.querySelectWithoutPassword,
     }); // This will return the user with the id passed in the url
   }
 
-  async update(id: number, data: CreateUserDto) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!user) {
-      throw new HttpException('Usuário não existe', HttpStatus.BAD_REQUEST);
-    }
+  async update(id: number, data: UpdateUserDto) {
+    await this.userExists(id);
 
     return this.prisma.user.update({
       where: {
         id,
       },
       data,
+      select: this.querySelectWithoutPassword,
     });
   }
 
   async remove(id: number) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id,
-      },
-    });
-
-    if (!user) {
-      throw new HttpException('Usuário não existe', HttpStatus.BAD_REQUEST);
-    }
+    await this.userExists(id);
 
     return this.prisma.user.delete({
       where: {
         id,
+      },
+      select: this.querySelectWithoutPassword,
+    });
+  }
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
       },
     });
   }
