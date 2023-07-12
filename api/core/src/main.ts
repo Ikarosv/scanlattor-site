86fa285d9d1696b28ddff278362957cc68d6ab55
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-12 15:05:03 +0000

    serverless

--- api/core/src/main.ts
+++ api/core/src/main.ts
@@ -8,6 +8,7 @@ import { ExpressFramework } from '@h4ad/serverless-adapter/lib/frameworks/expres
 import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/lib/adapters/aws';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const nestApp = await NestFactory.create(
@@ -15,23 +16,32 @@ async function bootstrap() {
     new ExpressAdapter(),
   );
   await nestApp.init()
+  const prismaService = nestApp.get(PrismaService);
+  await prismaService.enableShutdownHooks(nestApp)
   nestApp.useGlobalPipes(new ValidationPipe());
+  if (process.env.JWT_SECRET === 'secret') {
+    const port = process.env.PORT || 3001;
+    await nestApp.listen(port, '0.0.0.0');
+    console.log(
+      `\u001B[32m Server running on \u001B[0m http://localhost:${port}`,
+    );
+    return;
+  }
   const app = nestApp.getHttpAdapter().getInstance();
   return app
-  const port = process.env.PORT || 3001;
-  await nestApp.listen(port, '0.0.0.0');
-  console.log(
-    `\u001B[32m Server running on \u001B[0m http://localhost:${port}`,
-  );
 }
-// bootstrap();
-const expressFramework = new ExpressFramework();
-// the initialization of nestjs is asynchronous, so you can use the lazy framework.
-const framework = new LazyFramework(expressFramework, bootstrap);
 
-export const handler = ServerlessAdapter.new(null)
-  .setFramework(framework)
-  .setHandler(new DefaultHandler())
-  .setResolver(new PromiseResolver())
-  .addAdapter(new ApiGatewayV2Adapter())
-  .build();
+if (process.env.JWT_SECRET === 'secret') {
+  bootstrap();
+} else {
+  const expressFramework = new ExpressFramework();
+  // the initialization of nestjs is asynchronous, so you can use the lazy framework.
+  const framework = new LazyFramework(expressFramework, bootstrap);
+  
+  module.exports.handler = ServerlessAdapter.new(null)
+    .setFramework(framework)
+    .setHandler(new DefaultHandler())
+    .setResolver(new PromiseResolver())
+    .addAdapter(new ApiGatewayV2Adapter())
+    .build();
+}
