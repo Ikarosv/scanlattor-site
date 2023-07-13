e2c5db583e154aaee6b7208a086427b5af1a123c
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-13 17:29:04 +0000

    adiciona try

--- api/core/src/main.ts
+++ api/core/src/main.ts
@@ -11,24 +11,28 @@ import { ApiGatewayV2Adapter } from '@h4ad/serverless-adapter/lib/adapters/aws';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const nestApp = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(),
-  );
-  await nestApp.init()
-  const prismaService = nestApp.get(PrismaService);
-  await prismaService.enableShutdownHooks(nestApp)
-  nestApp.useGlobalPipes(new ValidationPipe());
-  if (process.env.JWT_SECRET === 'secret') {
-    const port = process.env.PORT || 3001;
-    await nestApp.listen(port, '0.0.0.0');
-    console.log(
-      `\u001B[32m Server running on \u001B[0m http://localhost:${port}`,
+  try {
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(),
     );
-    return;
+    await nestApp.init()
+    const prismaService = nestApp.get(PrismaService);
+    await prismaService.enableShutdownHooks(nestApp)
+    nestApp.useGlobalPipes(new ValidationPipe());
+    if (process.env.JWT_SECRET === 'secret') {
+      const port = process.env.PORT || 3001;
+      await nestApp.listen(port, '0.0.0.0');
+      console.log(
+        `\u001B[32m Server running on \u001B[0m http://localhost:${port}`,
+      );
+      return;
+    }
+    const app = nestApp.getHttpAdapter().getInstance();
+    return app
+  } catch (err) {
+    console.error(err)
   }
-  const app = nestApp.getHttpAdapter().getInstance();
-  return app
 }
 
 if (process.env.JWT_SECRET === 'secret') {
