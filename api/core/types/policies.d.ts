c8d7a2e2f16ae1f8671b2b712e043aa6237e57b3
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-21 04:55:27 +0000

    🏷️ type: cria tipagem da policies

---
+++ api/core/types/policies.d.ts
@@ -0,0 +1,9 @@
+import { AppAbility } from '../src/casl/abilities/AppAbility';
+
+interface IPolicyHandler {
+  handle(ability: AppAbility): boolean;
+}
+
+type PolicyHandlerCallback = (ability: AppAbility) => boolean;
+
+export type PolicyHandler = IPolicyHandler | PolicyHandlerCallback;
