2ff61a64b24d15ce4e341f2e567832827b75b97d
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-21 06:27:57 +0000

    ✨ feat: PoliciesGuard implementado

---
+++ api/core/src/policies-guard/policies-guard.guard.ts
@@ -0,0 +1,48 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { CaslAbilityFactory } from "../casl/casl-ability.factory/casl-ability.factory";
+import type { PolicyHandler } from "types/policies";
+import { CHECK_POLICIES_KEY } from "../decorators/checkPolicies.decorator";
+import type { AppAbility } from "src/casl/abilities/AppAbility";
+import { IS_PUBLIC_KEY } from "src/decorators/public.decorator";
+
+@Injectable()
+export class PoliciesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) { 
+      return true;
+    }
+
+    const policyHandlers =
+      this.reflector.get<PolicyHandler[]>(
+        CHECK_POLICIES_KEY,
+        context.getHandler(),
+      ) || [];
+
+    const { user } = context.switchToHttp().getRequest();
+    console.log(user);
+    
+    const ability = this.caslAbilityFactory.createForUser(user);
+
+    return policyHandlers.every((handler) =>
+      this.execPolicyHandler(handler, ability),
+    );
+  }
+
+  private execPolicyHandler(handler: PolicyHandler, ability: AppAbility) {
+    if (typeof handler === 'function') {
+      return handler(ability);
+    }
+    return handler.handle(ability);
+  }
+}
