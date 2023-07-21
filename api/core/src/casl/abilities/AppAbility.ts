31a3079bf6682e2f177c3a3a44693e36c7779445
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-21 06:24:04 +0000

    ♻️ refactor: separa o subjects

--- api/core/src/casl/abilities/AppAbility.ts
+++ api/core/src/casl/abilities/AppAbility.ts
@@ -1,19 +1,18 @@
 import { PureAbility } from '@casl/ability';
 import { PrismaQuery, Subjects } from '@casl/prisma';
 import { User, Manga, Chapter } from '@prisma/client';
-import { Action } from '../../enums/role.enums';
+import { Action } from '../../enums/actions.enums';
 
-export type AppAbility = PureAbility<
-  [
-    Action,
-    (
-      | Subjects<{
+export type subjects = Subjects<{
           User: User;
           Manga: Manga;
           Chapter: Chapter;
         }>
       | 'all'
-    ),
+
+export type AppAbility = PureAbility<
+  [
+    Action, subjects,
   ],
   PrismaQuery
 >;
