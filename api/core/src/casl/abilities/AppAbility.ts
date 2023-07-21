fc86d7419617b1a154b9c75814014a0da6880362
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-21 02:48:18 +0000

    ♻️ refactor: corrige importação

---
+++ api/core/src/casl/abilities/AppAbility.ts
@@ -0,0 +1,19 @@
+import { PureAbility } from '@casl/ability';
+import { PrismaQuery, Subjects } from '@casl/prisma';
+import { User, Manga, Chapter } from '@prisma/client';
+import { Action } from '../../enums/role.enums';
+
+export type AppAbility = PureAbility<
+  [
+    Action,
+    (
+      | Subjects<{
+          User: User;
+          Manga: Manga;
+          Chapter: Chapter;
+        }>
+      | 'all'
+    ),
+  ],
+  PrismaQuery
+>;
