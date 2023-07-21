fbcba0d7a86c7d7598f87e90ae307db194ac186a
Ikaro Vieira <ikarovieiraa@gmail.com>
2023-07-21 06:21:41 +0000

    🏷️ type: adiciona void no handler

--- api/core/types/policies.d.ts
+++ api/core/types/policies.d.ts
@@ -1,7 +1,7 @@
 import { AppAbility } from '../src/casl/abilities/AppAbility';
 
 interface IPolicyHandler {
-  handle(ability: AppAbility): boolean;
+  handle(ability: AppAbility): boolean | void;
 }
 
 type PolicyHandlerCallback = (ability: AppAbility) => boolean;
